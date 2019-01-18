05fb83d2db1cdd5b9122fa383da9a3e062432c5a
Yerome <vinet.jerome@gmail.com>
2019-01-18 15:10:34 +0100

    recup userprofil

--- frontend/src/app/user.ts
+++ frontend/src/app/user.ts
@@ -1,23 +1,65 @@
 export class User {
-  id?: number;
-  login: string;
-  password: string;
-  id_UserTypes: number;
-  id_Profiles: number;
-  usertypes:
-    {
-      id: number;
-      label: string
-    };
-  profile:
-    {
-      id: number;
-      name: string;
-      firstName: string;
-      email: string;
-      active: number;
-      createdDate: string;
-      updatedDate: null;
-      insee_Cities: string
-    }
+  // id?: number;
+  // login: string;
+  // password: string;
+  // id_UserTypes: number;
+  // id_Profiles: number;
+  // usertypes:
+  //   {
+  //     id: number;
+  //     label: string
+  //   };
+  // profile:
+  //   {
+  //     id: number;
+  //     name: string;
+  //     firstName: string;
+  //     email: string;
+  //     active: number;
+  //     createdDate: string;
+  //     updatedDate: null;
+  //     insee_Cities: string
+  //   }
+
+
+  public static fromJson(json: Object): User {
+    return new User(
+      json['id'],
+      json['login'],
+      json['usertypes'],
+      json['usertypes'],
+      json['usertypes'],
+      json['usertypes'],
+      json['usertypes'],
+      json['usertypes'],
+      json['usertypes']);
+  }
+
+  constructor(public id: number,
+              public login: string,
+              public password: string,
+              public id_UserTypes: number,
+              public id_Profiles: number,
+              public usertypes:
+                {
+                  id: number;
+                  label: string
+                },
+              public profile:
+                {
+                  id: number;
+                  name: string;
+                  firstName: string;
+                  email: string;
+                  active: number;
+                  createdDate: string;
+                  updatedDate: null;
+                  insee_Cities: string
+                },
+              // public author: string,
+              public title: string,
+              public body: string,) {
+  }
+
+
 }
