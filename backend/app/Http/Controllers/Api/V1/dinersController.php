b7cca0e6c114f4fd50d98fda7aa56f5cde718619
Unknown <Christo.tpe@gmail.com>
2019-02-25 19:23:48 +0100

    Fonction Back dinersController et commentaire du code

--- backend/app/Http/Controllers/Api/V1/dinersController.php
+++ backend/app/Http/Controllers/Api/V1/dinersController.php
@@ -13,35 +13,68 @@ class dinersController extends Controller
 {
     use Helpers;
 
+    /**
+     * @return Diners[]|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     * commentaire cg: récupération des diners dont la date est supérieur a la date du jour
+     */
     public function getDiners(){
         return Diners::with('place', 'theme', 'user', 'usersdiners')->where("date", ">=", date("Y-m-d h:i:s"))->get();
     }
 
+    /**
+     * @param $id
+     * @return Diners|Diners[]|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     * récupération d'un diner
+     */
     public function getDiner($id){
         return Diners::with('place','theme','user', 'usersdiners')->where('date','>=', 'now()')->orderBy('date','asc')->find($id);
     }
 
+    /**
+     * @param $id
+     * @return Diners|Diners[]|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     * commentaire cg : récupération des anciens diners (diners dont la date du diners est inférieur a la date du jour)
+     */
     public function getOldDiner($id){
         return Diners::with('place','theme','user', 'usersdiners')->where('date','<', 'now()')->orderBy('date','desc')->find($id);
     }
 
+    /**
+     * @param Request $request
+     * @return Diners[]|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     * commentaire cg : récupération des diners dont je suis l'organisateur
+     */
     public function getMyOwnDiners(Request $request){
         return Diners::with('place', 'theme', 'user', 'usersdiners')->where(['id_Organisator' => $request->user_id])->get();
     }
 
+    /***
+     * @param Request $request
+     * @return Diners[]|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     * commentaire cg: récupération des diners auquels j'ai participé
+     */
     public function getMyDiners(Request $request){
         return Diners::with('place','theme','user', 'usersdiners')->where('date','>=','now()')->whereHas('usersdiners' , function ($query) use ($request) {
             $query->where('id_Users', '=', $request->user_id);
         })->get();
     }
 
+    /**
+     * @param Request $request
+     * @return Diners[]|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     * commentaire cg : récupération des anciens diners auquels j'ai participé
+     */
     public function getMyOldDiners(Request $request){
         return Diners::with('place','theme','user', 'usersdiners')->where('date','<','now()')->whereHas('usersdiners' , function ($query) use ($request) {
             $query->where('id_Users', '=', $request->user_id);
         })->get();
     }
 
-
+    /**
+     * @param Request $request
+     * @return string
+     * commentaire cg: création du diner, le request est renseigner dans le tableau attribut
+     */
     public function createDiner(Request $request){
         $attribut = [];
 
@@ -69,13 +102,65 @@ class dinersController extends Controller
 
     }
 
+    /**
+     * @param $id
+     * @return string
+     * Commentaire cg : Cette fonction ne supprime pas le diner mais renseigne la date "deleted_at"
+     */
     public function deleteDiner($id){
 
         try{
-            Diners::deleted($id);
+            $diner = Diners::find($id);
+            $diner->delete();
+
         }catch (\PDOException $e){
             return $e->getMessage();
         }
     }
+
+    public function updateDiner(Request $request){
+
+        try{
+            DB::beginTransaction();
+            $diners = new Diners();
+
+            if (isset($request->id)){
+                $diners = Diners::find($request->id);
+            }
+
+            if (isset($diners->id)) {
+                //return $request;
+                foreach ($request->all() as $key => $param) {
+
+                    if ($key != 'id') {
+                        $diners->$key = $param;
+
+                    }
+                }
+                $diners->save();
+
+                DB::commit();
+                return $this->api->get('diners/' . $diners->id);
+            } ELSE {
+                $this->response->errorBadRequest();
+            }
+
+        }catch (\PDOException $e){
+           return $e;
+           DB::rollBack();
+        }
+
+    }
+
+    /**
+     * @return mixed
+     * commentaier cg: récupération des diners supprimés
+     */
+    public function getDeletedDiners(){
+        return Diners::with('place', 'theme', 'user', 'usersdiners')->onlyTrashed()->get();
+
+    }
+
+
 
 }
