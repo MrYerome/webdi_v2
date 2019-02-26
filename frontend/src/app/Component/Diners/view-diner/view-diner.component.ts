581f639c1f9e3aa401260c8acdecb73e2507b35e
Unknown <Christo.tpe@gmail.com>
2019-02-26 20:32:53 +0100

    view diner

---
+++ frontend/src/app/Component/Diners/view-diner/view-diner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-diner',
+  templateUrl: './view-diner.component.html',
+  styleUrls: ['./view-diner.component.css']
+})
+export class ViewDinerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
