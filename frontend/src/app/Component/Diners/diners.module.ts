40cda676c394b07b4a686922765a65674bd5eee4
Unknown <Christo.tpe@gmail.com>
2019-02-26 11:00:15 +0100

    Réglage problème routing back

---
+++ frontend/src/app/Component/Diners/diners.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListDinersComponent } from './list-diners/list-diners.component';
+
+@NgModule({
+  declarations: [ListDinersComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DinersModule { }
