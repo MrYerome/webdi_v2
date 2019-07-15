c6f44ccb2231f5c18502a7a87e3f4e8f842b24be
Unknown <Christo.tpe@gmail.com>
2019-07-15 15:14:51 +0200

    dropdown mon compte

--- frontend/src/app/Component/navbar/navbar.component.ts
+++ frontend/src/app/Component/navbar/navbar.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
-import{AuthService} from "../../Services/auth.service";
+import {AuthService} from '../../Services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  host: {
+      '(document:click)': 'hideDropdown($event)',
+  },
 })
 export class NavbarComponent implements OnInit {
+    public showDropdown:boolean = false;
 
-
-  public loggedIn: boolean;
+    public loggedIn: boolean;
 
   constructor(
     private Auth: AuthService,
     private router: Router,
+    private _eref: ElementRef
   ) { }
 
   ngOnInit() {
-    this.Auth.authStatus.subscribe(value => { this.loggedIn = value; console.log(value);});
+    this.Auth.authStatus.subscribe(value => { this.loggedIn = value; console.log(value); });
+    this.showDropdown = false;
   }
 
   logout(event: MouseEvent) {
@@ -26,6 +31,12 @@ export class NavbarComponent implements OnInit {
     this.Auth.changeAuthStatus(false);
     sessionStorage.clear();
     this.router.navigateByUrl('/accueil');
+  }
+
+  hideDropdown($e) {
+    if (!this._eref.nativeElement.contains(event.target)) {
+        this.showDropdown = false;
+    } // or some similar check
   }
 
 
